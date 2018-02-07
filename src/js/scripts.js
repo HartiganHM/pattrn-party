061a4d7e78b753df51981c1f9a968f9718677fa2
Hugh Hartigan <Hartigan.HM@gmail.com>
2018-02-06 21:24:44 -0700

    Build toggleActiveContent

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,6 +4,17 @@ const toggleActiveTab = (event) => {
   if (!$(clickedTab).hasClass('.active-tab-header')) {
     $('.tab-header').removeClass('active-tab-header')
     $(clickedTab).addClass('active-tab-header')
+
+    toggleActiveContent(clickedTab);
+  }
+}
+
+const toggleActiveContent = (tab) => {
+  const clickedContent = $(tab).siblings('.tab-content')[0]
+
+  if (!$(clickedContent).hasClass('.active-content')) {
+    $('.tab-content').removeClass('active-content')
+    $(clickedContent).addClass('active-content')
   }
 }
 
